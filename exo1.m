c031bf16d29ea31da3c3172639a5a5585d6fee9e
Frederik Plahl <plfr1013@h-ka.de>
2022-04-17 23:32:48 +0200

    Add bovw classifier

--- exo1.m
+++ exo1.m
@@ -1,21 +1,21 @@
+clc;
+clear;
 
 Element = ["bottle","grid","leather"];
 for type = Element
-%type = "bottle";
-    d = dir("C:\Users\Caroline\mvtec_anomaly_detection\00_data\" + type + "\train\good");
+    d = dir("/Users/frederikplahl/mvtec_anomaly_detection/00_data/" + type + "/train/good");
     Nbr = length(d([d.isdir]==0));
-    for i=0:Nbr -1
+    for i=0:Nbr-1
         Nom = "B" + i;
         if (i < 10)
             i = "00" + i;
         elseif (i <100)
             i = "0" + i;
         end
-        B = "C:\Users\Caroline\mvtec_anomaly_detection\00_data\" + type + "\train\good\"  + i + ".png";
-        Nom = imread (B);
+        B = "/Users/frederikplahl/mvtec_anomaly_detection/00_data/" + type + "/train/good/"  + i + ".png";
+        image = imread(B);
     end
 end
-imshow (Nom);
-
+imshow(image);
 
 
